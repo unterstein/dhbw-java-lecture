bea787fcffdff3d5fec24c265ab773dcfdf183e5
Johannes Unterstein <unterstein@me.com>
2016-05-25 06:42:37 +0200

    this-super-stuff

---
+++ src/ba/java/oo/ThisSuperBeispiel.java
@@ -0,0 +1,29 @@
+package ba.java.oo;
+
+public class ThisSuperBeispiel {
+
+  private int numberOfSomething;
+
+  private String stringOfSomething;
+
+  public ThisSuperBeispiel() {
+    // stringOfSomething ist Variable von eigener Klasse. Aufrufe müssen nicht explizit mit this deklariert werden!
+    stringOfSomething = "Hello!";
+    // könnn es aber  ;)
+    this.stringOfSomething = "Hi!";
+  }
+
+  public void setNumberOfSomething(int numberOfSomething) {
+    // In diesem Szenario muss sinnvollerweise die Variablen-Zuweisung mit this. deklariert werden
+    this.numberOfSomething = numberOfSomething;
+  }
+
+  public String print() {
+    // toString ist Methode von Superklasse. Aufrufe müssen nicht explizit mit "super." deklariert werden!
+    System.out.println(toString());
+  }
+
+  public static void main(String[] args) {
+    new ThisSuperBeispiel();
+  }
+}
