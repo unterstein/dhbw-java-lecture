9eecca486a7fd46a5544f12d7c8be0a61778183a
Johannes Unterstein <unterstein@me.com>
2016-06-01 06:46:29 +0200

    even more strings

--- src/ba/java/uebungen/StringUebung.java
+++ src/ba/java/uebungen/StringUebung.java
@@ -9,5 +9,18 @@ public class StringUebung {
     System.out.println(string1);
     string1 = string1.concat("Welt!");
     System.out.println(string1);
+
+    System.out.println("----");
+
+    String string2 = "Hallo1";
+    String string3 = "Hallo".concat("1");
+    System.out.println(string2 == string3);
+
+    String string4 = "Hallo1".replace("1", "2");
+    String string5 = "Hallo2";
+    String string6 = "Hallo2";
+    System.out.println(string4 == string5);
+    System.out.println(string4 == string6);
+    System.out.println(string5 == string6);
   }
 }
