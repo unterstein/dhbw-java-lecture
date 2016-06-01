7e5873199c326171f28c23b5a09f3fac4fdd7272
Johannes Unterstein <unterstein@me.com>
2016-06-01 07:29:52 +0200

    more java 8 features

--- src/ba/java/uebungen/Java8Uebung.java
+++ src/ba/java/uebungen/Java8Uebung.java
@@ -2,6 +2,8 @@ package ba.java.uebungen;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Java8Uebung {
 
@@ -17,5 +19,30 @@ public class Java8Uebung {
     liste.stream() // Keyword für Java8 Collections
         .map(Java8Uebung::methodRef) // .map erwartet Rückgabewert -> return String
         .forEach(System.out::println); // .forEach ist das gleiche wie .map, allerdings ohne Rückgabewert -> return void
+
+    Optional<Integer> result = liste.stream() // .reduce liefert ein Optional zurück! "Kann was drin sein, muss aber nicht"
+        .map(string -> string.length()) // .map funktioniert auch mit Lambdas
+        .reduce((a, b) -> a + b);// .reduce reduziert zwei Werte aus der Liste zu einem, basierend auf Lambda
+    System.out.println(result);
+
+    boolean anyMatch = liste.stream()
+        .anyMatch(s -> Integer.valueOf(s) % 2 == 0); // checks mit .anyMatch
+    System.out.println(anyMatch);
+
+    boolean allMatch = liste.stream()
+        .allMatch(s -> Integer.valueOf(s) % 2 == 0); // checks mit .allMatch
+
+    System.out.println(allMatch);
+
+    Optional<String> reduce = liste.stream()
+        .filter(s -> Integer.valueOf(s) % 2 == 0) // Mit .filter auch filtern auch möglich
+        .reduce((a, b) -> a + ", " + b); // .reduce kann auch auf Strings angewendet werden
+    System.out.println(reduce.get()); // optional.get() liefert eigentlichen Wert zurück
+
+    // Optionals
+    Optional<String> optional = Optional.empty();
+    System.out.println(optional);
+    System.out.println(optional.isPresent());
+    System.out.println(optional.get()); // .get auf empty Optionals geht nicht...
   }
 }
