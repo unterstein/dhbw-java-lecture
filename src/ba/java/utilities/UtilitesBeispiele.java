bc9929873c848d5041614473d93f8ebdfdd7d80a
Johannes Unterstein <unterstein@me.com>
2016-06-01 08:12:34 +0200

    utilities

--- src/ba/java/utilities/UtilitesBeispiele.java
+++ src/ba/java/utilities/UtilitesBeispiele.java
@@ -4,15 +4,21 @@ import java.util.Random;
 
 public class UtilitesBeispiele {
 
-  private void utilities() {
+  private static void utilities() {
     Random random = new Random(System.currentTimeMillis() % 14930352);
     for (int i = 0; i < 10; i++) {
       System.out.println(random.nextInt());
     }
   }
 
-  private void system() {
-    System.getProperty("");
-    // Runtime.getRuntime().exec(command)
+  private static void system() {
+    System.out.println(System.getProperty("line.separator"));
+    System.out.println(System.getenv("path"));
+     Runtime.getRuntime().exec("say i can speak!");
+  }
+
+  public static void main(String[] args) {
+    utilities();
+    system();
   }
 }
