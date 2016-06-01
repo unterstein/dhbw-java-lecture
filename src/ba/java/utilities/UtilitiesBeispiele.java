15ed01f3fdcc8869e18f9349998a5af2763dd4ea
Johannes Unterstein <unterstein@me.com>
2016-06-01 08:37:56 +0200

    utilities

--- src/ba/java/utilities/UtilitiesBeispiele.java
+++ src/ba/java/utilities/UtilitiesBeispiele.java
@@ -3,13 +3,18 @@ package ba.java.utilities;
 import java.io.IOException;
 import java.util.Random;
 
-public class UtilitesBeispiele {
+public class UtilitiesBeispiele {
+
+  private static Random random = new Random(System.currentTimeMillis() % 14930352);
 
   private static void utilities() {
-    Random random = new Random(System.currentTimeMillis() % 14930352);
     for (int i = 0; i < 10; i++) {
-      System.out.println(random.nextInt());
+      System.out.println(nextRandomInt());
     }
+  }
+
+  public static int nextRandomInt() {
+    return random.nextInt();
   }
 
   private static void system() {
