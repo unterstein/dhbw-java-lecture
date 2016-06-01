10191d87fa206c0d371689252fa52aaf4e5df097
Johannes Unterstein <unterstein@me.com>
2016-06-01 08:37:46 +0200

    exceptions

---
+++ src/ba/java/uebungen/ExceptionUebung.java
@@ -0,0 +1,38 @@
+package ba.java.uebungen;
+
+import ba.java.utilities.UtilitiesBeispiele;
+
+import java.io.FileNotFoundException;
+
+public class ExceptionUebung {
+
+  private static class MeineException extends RuntimeException {
+
+    public MeineException(String text) {
+      super(text);
+    }
+  }
+
+  private static void trycatch() {
+    try {
+      test();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (ArrayIndexOutOfBoundsException e) {
+      e.printStackTrace();
+    } catch (NullPointerException e) {
+      // Bitte immer etwas mit einer Exception machen und nicht einfach schlucken!!
+      // e.printStackTrace();
+    } catch (Exception e) { // Muss ganz unten stehen!!
+
+    }
+  }
+
+  private static void test() throws FileNotFoundException, ArrayIndexOutOfBoundsException, NullPointerException {
+    if (UtilitiesBeispiele.nextRandomInt() % 2 == 0) {
+      throw new RuntimeException("hallo");
+    }
+    throw new MeineException("Meins");
+  }
+
+}
